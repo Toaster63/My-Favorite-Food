632487aa3ef341324818696e1550acae341d69a7
Toaster63 <maggie_iniestra@hotmail.com>
2024-05-21 19:58:36 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function changeImageOver1()
+{document.getElementById("grab").src="pizzafancy.jpg"}
+function changeImageLeave1()
+{document.getElementById("grab").src="pizzagrab.jpg"}
+function changeImageOver2()
+{document.getElementById("cheese").src="pepperoni.jpg"}
+function changeImageLeave2()
+{document.getElementById("cheese").src="cheesepizza.jpg"}
